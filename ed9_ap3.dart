c9494fcfe7550972bb473c94dfbe396b3c8e34ae
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 20:44:04 -0300

    AP3 Concluida

--- ed9_ap3.dart
+++ ed9_ap3.dart
@@ -1,1 +1,15 @@
-void main() {}
+Future<void> main() async {
+  print('Iniciando lancamento');
+
+  await contagemRegressiva();
+
+  await Future.delayed(Duration(seconds: 1));
+  print('Foguete lancado!');
+}
+
+Future<void> contagemRegressiva() async {
+  for (var i = 10; i > 0; i--) {
+    await Future.delayed(Duration(seconds: 1));
+    print(i);
+  }
+}
