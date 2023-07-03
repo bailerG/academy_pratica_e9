d99a4da88d09d2cccd73797f6df137dcf10f7dc1
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 20:57:55 -0300

    AP4 COncluida

--- ed9_ap4.dart
+++ ed9_ap4.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'dart:io';
+
+Future<void> main() async {
+  String? input = "";
+
+  print("Digite qual dado você busca:");
+  input = stdin.readLineSync();
+
+  print("Buscando dado...");
+  await buscarDado(input!);
+
+  await Future.delayed(Duration(milliseconds: 500));
+  print("Fim da busca.");
+}
+
+Future<void> buscarDado(String input) async {
+  await Future.delayed(Duration(seconds: 2));
+  print("Aqui está: $input.dado");
+}
