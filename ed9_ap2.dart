ac696c5f90bba59efa44d05eda43eece27788945
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 20:36:53 -0300

    AP2 Concluida

--- ed9_ap2.dart
+++ ed9_ap2.dart
@@ -1,1 +1,24 @@
-void main() {}
+Future<void> main() async {
+  List<String> urls = [
+    'https://example.com/imagem1.jpg',
+    'https://example.com/imagem2.jpg',
+    'https://example.com/imagem3.jpg',
+  ];
+
+  await baixarImagens(urls);
+}
+
+Future<void> simulaDownload(String url) async {
+  await Future.delayed(Duration(seconds: 1));
+  print("Imagem $url baixada com sucesso!");
+}
+
+Future<void> baixarImagens(List<String> urls) async {
+  print("Baixando imagens...");
+
+  for (String url in urls) {
+    await simulaDownload(url);
+  }
+
+  print("Download concluido!");
+}
